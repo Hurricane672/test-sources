fb12341723360faf80c018f804d3af5c2841b9d5
EternalMemory672 <274588969@qq.com>
2018-05-19 16:13:05 +0800

    2018.5.19

---
+++ c++/斐波那契数列.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a[50];
+	a[0]=1;
+	a[1]=1;
+	int k;
+	cin>>k;
+	for(int c=2; c<k; c++)
+		{
+			a[c]=a[c-1]+a[c-2];
+		}
+	for(int c=0; a[c]!=NULL; c++)
+		{
+			if((c+1)%5==0)
+				{
+					cout<<a[c]<<endl;
+				}
+			else{
+				cout<<a[c]<<"\t";
+			}
+		}
+	cout<<a[k];
+}
---
+++ test/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+//int plus(int a,int b);
+int main()
+{
+    cout << "Hello, World!" << endl;
+    return 0;
+}
