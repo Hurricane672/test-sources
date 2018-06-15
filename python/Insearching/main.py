9c10ebb8e394dadfd8156156ab8ac40d2bebe486
EternalMemory672 <274588969@qq.com>
2018-06-16 00:33:36 +0800

    2018

--- python/Insearching/main.py
+++ python/Insearching/main.py
@@ -1,2 +1,29 @@
+#-*- coding:utf-8 -*-
 import time
-import Py
+import PyQt5
+import whois
+import requests
+import urllib3
+import sys
+import os
+import queue
+import threading
+from bs4 import BeautifulSoup as ba
+def main():
+
+    path = os.path.split(os.path.realpath(__file__))[0]
+    urlPath = path+"\\urls.txt"
+    searchPath = path+"\\search.txt"
+    # http://www.baidu.com/s?wd=example&pn=x
+    with open(urlPath) as u:
+        urls = u.read().splitlines()
+    with open(searchPath,"r+",encoding="utf-8") as s:
+        searchs = s.read().splitlines()
+    print(urls)
+    print(searchs)
+
+if __name__ == '__main__':
+    main()
+#for url in urls:
+#    r = requests.get(url)
+#    print(r.status_code,r.content)
