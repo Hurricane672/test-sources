aeb5228044f22bf7a40d8a5bd6a3983ae9af06c1
EternalMemory672 <274588969@qq.com>
2018-06-16 01:40:41 +0800

    2018.6.16

--- python/Insearching/main.py
+++ python/Insearching/main.py
@@ -1,5 +1,6 @@
-#-*- coding:utf-8 -*-
+#coding=utf-8
 import time
+import re
 import PyQt5
 import whois
 import requests
@@ -8,9 +9,10 @@ import sys
 import os
 import queue
 import threading
+import urllib
 from bs4 import BeautifulSoup as ba
-def main():
 
+def getInfo():
     path = os.path.split(os.path.realpath(__file__))[0]
     urlPath = path+"\\urls.txt"
     searchPath = path+"\\search.txt"
@@ -19,9 +21,41 @@ def main():
         urls = u.read().splitlines()
     with open(searchPath,"r+",encoding="utf-8") as s:
         searchs = s.read().splitlines()
-    print(urls)
-    print(searchs)
+    print("\nURLs:")
+    for i in urls:
+        print("  [+] %s"%i)
+    print("\nKeyWords: ")
+    for i in searchs:
+        print("  [+] %s"%i)
+    return(urls,searchs,path)
 
+def getUrls(url):
+    s=urllib.request.urlopen(url).read()
+    ss=s.replace(" ","")
+    urls=re.findall(r"<a.*?href=.*?<\/a>",ss,re.I)
+    for i in urls:
+        print (i)
+    else:
+        print ("[-]Over")
+
+def getPages(urls):
+    #for i in urls:
+    i = "http://www.baidu.com/s?wd=" + "人际交往密切程度有多高"
+    r = requests.get(i)
+    return getUrls(i)
+
+def main():
+    while (True):
+        print("Collecting informations...")
+        (urls,searchs,path) = getInfo()
+        x = input("Get again?(y/n)")
+        if x == "y":
+            continue
+        if x == "n":
+            break
+        else:
+            continue
+    getPages(urls)
 if __name__ == '__main__':
     main()
 #for url in urls:
